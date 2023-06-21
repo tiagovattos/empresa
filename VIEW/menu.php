eeb8efc5c84a48b7520cae03c48c1ae4da898e25
tiagovattos <tiagovattosz@gmail.com>
2023-06-21 17:25:54 -0300

    Adicao menu rodape

---
+++ VIEW/menu.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <title>Menu</title>
+</head>
+
+<body>
+    <nav>
+        <div class="nav-wrapper purple darken-4">
+            <a href="#" class="brand-logo right">Logo</a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a href="sass.html">Clientes</a></li>
+                <li><a href="badges.html">Categorias</a></li>
+                <li><a href="collapsible.html">Produtos</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+
+</html>
---
+++ VIEW/rodape.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+
+
+    <title>Rodape</title>
+</head>
+
+<body>
+    <footer class="page-footer purple darken-4">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Empresa</h5>
+                    <p class="grey-text text-lighten-4">Fique ligado em nossas redes sociais!</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Redes Sociais</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="https://www.instagram.com/"><i class="fab fa-instagram"></i> Instagram</a></li>
+                        <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/"><i class="fab fa-facebook"></i> Facebook</a></li>
+                        <li><a class="grey-text text-lighten-3" href="https://www.twitter.com/"><i class="fab fa-twitter"></i> Twitter</a></li>
+                        <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i> Linkedin</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2023 Tiago e Rafael
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
